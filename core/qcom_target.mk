371d6a1cdab5e25ca48d35fa5025e9c3da6cd0df
spezi77 <spezi7713@gmail.com>
2014-12-07 12:52:59 +0100

    qcom make: prepare to pair with Evervolv
    Change-Id: I725b4c8b542257b8ccfe97b4dae568b9e2104a23

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -28,17 +28,47 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     LOCAL_GLOBAL_CPPFLAGS += -DQCOM_BSP_LEGACY
     endif
 
-$(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
-$(call qcom-set-path-variant,CAMERA,camera)
-$(call project-set-path,qcom-display,hardware/qcom/display-caf/$(TARGET_BOARD_PLATFORM))
-$(call qcom-set-path-variant,GPS,gps)
-$(call project-set-path,qcom-media,hardware/qcom/media-caf/$(TARGET_BOARD_PLATFORM))
-$(call qcom-set-path-variant,SENSORS,sensors)
+    ifneq ($(filter msm8084,$(TARGET_BOARD_PLATFORM)),)
+        #This is for 8084 based platforms
+        QCOM_AUDIO_VARIANT := audio-caf/msm8084
+        QCOM_DISPLAY_VARIANT := display-caf/msm8084
+        QCOM_MEDIA_VARIANT := media-caf/msm8084
+    else ifneq ($(filter msm8610 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
+        #This is for 8974 based (and B-family) platforms
+        QCOM_AUDIO_VARIANT := audio-caf/msm8974
+        QCOM_DISPLAY_VARIANT := display-caf/msm8974
+        QCOM_MEDIA_VARIANT := media-caf/msm8974
+    else
+        QCOM_AUDIO_VARIANT := audio-caf/msm8960
+        # Enables legacy repos to be handled
+        ifeq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+            QCOM_DISPLAY_VARIANT := display-legacy
+            QCOM_MEDIA_VARIANT := media-legacy
+        else
+            QCOM_DISPLAY_VARIANT := display-caf/msm8960
+            QCOM_MEDIA_VARIANT := media-caf/msm8960
+        endif
+    endif
+
 else
-$(call project-set-path,qcom-audio,hardware/qcom/audio)
-$(call qcom-set-path-variant,CAMERA,camera)
-$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
-$(call qcom-set-path-variant,GPS,gps)
-$(call project-set-path,qcom-media,hardware/qcom/media)
-$(call qcom-set-path-variant,SENSORS,sensors)
+    # QSD8K doesn't use QCOM_HARDWARE flag
+    ifneq ($(filter qsd8k,$(TARGET_BOARD_PLATFORM)),)
+        QCOM_AUDIO_VARIANT := audio-caf/msm8960
+    else
+        QCOM_AUDIO_VARIANT := audio
+    endif
+    ifeq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+        QCOM_DISPLAY_VARIANT := display-legacy
+        QCOM_MEDIA_VARIANT := media-legacy
+    else
+        QCOM_DISPLAY_VARIANT := display
+        QCOM_MEDIA_VARIANT := media
+    endif
 endif
+
+$(call project-set-path,qcom-audio,hardware/qcom/$(QCOM_AUDIO_VARIANT))
+$(call project-set-path,qcom-display,hardware/qcom/$(QCOM_DISPLAY_VARIANT))
+$(call project-set-path,qcom-media,hardware/qcom/$(QCOM_MEDIA_VARIANT))
+$(call qcom-set-path-variant,CAMERA,camera)
+$(call qcom-set-path-variant,GPS,gps)
+$(call qcom-set-path-variant,SENSORS,sensors)
