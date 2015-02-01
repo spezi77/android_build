e90327a5b369d818a130ec3f972f1e0bb5b96493
spezi77 <spezi7713@gmail.com>
2015-02-01 19:16:24 +0100

    Qcom legacy targets: fix a problem with missed out QSD8K libs
    Change-Id: Id07934bd804b5868f5813f2de250b47441e410d5
    
    Conflicts:
    	core/qcom_target.mk

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -1,21 +1,37 @@
 # Target-specific configuration
 
+# Populate the qcom hardware variants in the project pathmap.
+define qcom-set-path-variant
+$(call project-set-path-variant,qcom-$(2),TARGET_QCOM_$(1)_VARIANT,hardware/qcom/$(2))
+endef
+define ril-set-path-variant
+$(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))
+endef
+
 # Enable DirectTrack on QCOM legacy boards
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
     TARGET_GLOBAL_CFLAGS += -DQCOM_HARDWARE
     TARGET_GLOBAL_CPPFLAGS += -DQCOM_HARDWARE
 
-    ifeq ($(TARGET_USES_QCOM_BSP),true)
+    ifneq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+        TARGET_USES_QCOM_BSP := true
         TARGET_GLOBAL_CFLAGS += -DQCOM_BSP
         TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP
     endif
 
+    TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
     # Enable DirectTrack for legacy targets
     ifneq ($(filter caf caf-bfam legacy,$(TARGET_QCOM_AUDIO_VARIANT)),)
-        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-            TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
-            TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
+        TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
+        TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
+    endif
+        ifneq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+            # Enable legacy graphics functions
+            TARGET_USES_QCOM_BSP_LEGACY := true
+            TARGET_GLOBAL_CFLAGS += -DQCOM_BSP_LEGACY
+            TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP_LEGACY
         endif
     endif
 
@@ -47,7 +63,7 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 else
     # QSD8K doesn't use QCOM_HARDWARE flag
     ifneq ($(filter qsd8k,$(TARGET_BOARD_PLATFORM)),)
-        QCOM_AUDIO_VARIANT := audio-caf/msm8960
+        QCOM_AUDIO_VARIANT := audio-legacy
     else
         QCOM_AUDIO_VARIANT := audio
     endif
@@ -58,5 +74,16 @@ else
         QCOM_DISPLAY_VARIANT := display
         QCOM_MEDIA_VARIANT := media
     endif
-    $(call ril-set-path-variant,ril)
 endif
+
+$(call project-set-path,qcom-audio,hardware/qcom/$(QCOM_AUDIO_VARIANT))
+$(call project-set-path,qcom-display,hardware/qcom/$(QCOM_DISPLAY_VARIANT))
+$(call project-set-path,qcom-media,hardware/qcom/$(QCOM_MEDIA_VARIANT))
+ifeq ($(USE_DEVICE_SPECIFIC_CAMERA),true)
+$(call project-set-path,qcom-camera,$(TARGET_DEVICE_DIR)/camera)
+else
+$(call qcom-set-path-variant,CAMERA,camera)
+endif
+$(call qcom-set-path-variant,GPS,gps)
+$(call qcom-set-path-variant,SENSORS,sensors)
+$(call ril-set-path-variant,ril)
