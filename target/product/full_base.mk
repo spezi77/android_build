faf9555f36fe1ae48e3e8e7cbf8006f2acf0a689
spezi77 <spezi77@gmx.com>
2015-12-31 12:54:20 +0100

    Kill Bloat with Fire! (got inspired by Cl3Kener)
    Change-Id: I99b5f8e1b6c9821c7249251bfb7dfdcd70d7191f
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -27,16 +27,10 @@ PRODUCT_PACKAGES := \
 PRODUCT_LOCALES := en_US
 
 # Include drawables for all densities
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_CONFIG := normal hdpi
 
 # Get some sounds
 #$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-
-# Get the TTS language packs
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
-
-# Get a list of languages.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
